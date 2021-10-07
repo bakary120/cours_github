97ee5f6152e9455f344e9dfe7e72bc37cd235dc2
bakary120 <traorebakary120@gmail.com>
2021-10-07 14:56:52 +0200

    bak

---
+++ bakary/bak.js
@@ -0,0 +1,43 @@
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+window.onscroll = function () {
+  navs();
+};
+
+function navs() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// formulaire de contact envoyer message
+
+function envoyer() {
+  let noms = "sabrina";
+  let prenoms = "hajar";
+  let mail = "mail@gmail.com";
+  let messages = bonjour;
+
+  let nom = document.getElementById("idnom").value;
+  let prenom = document.getElementById("").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("").value;
+
+  if (
+    nom == noms &&
+    prenom == prenoms &&
+    email == mail &&
+    message == messages
+  ) {
+    alert("message envoyer");
+    return true;
+  } else if (nom == "" && prenom == "" && email == "" && messages == "") {
+    alert("champsVide");
+    return false;
+  } else {
+    alert("erreur de saisie");
+    return false;
+  }
+}
